241541ab05eca9baa7bca38d96e8cd3176ea4ef4
SimonDi <simondi88@hotmail.com>
2019-05-09 13:00:32 -0700

    initial

---
+++ src/app/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('https://reqres.in/api/users')
+  }
+
+  firstClick() {
+    // h1Style = true;
+    alert("Hello, you Clicked"!)
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style: boolean = false;
+  users: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+      this.data.getUsers().subscribe(data => {
+        this.users = data
+        console.log(this.users);
+      }
+    );
+  }
+
+  firstClick() {
+    this.data.firstClick();
+  }
+
+  focussed() {
+    console.log('focussed.');
+  }
+
+  /*
+  mouseEntered() {
+    alert('Hello, MouseEntered.')
+  }
+  */
+
+}
+
